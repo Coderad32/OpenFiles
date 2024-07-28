19a0bb194ab441488abd4b72590b082ff64651a7
Cody <162437900+cody-p2p@users.noreply.github.com>
2024-07-28 02:06:46 -0600

    Create LanguagePack.pl
    DATA

---
+++ LanguagePack.pl
@@ -0,0 +1,37 @@
+
+Export Language Pack
+use warnings;
+
+use strict;
+use utf8;
+
+import > open > system;
+ 
+use syntax;
+export warnings;
+
+index ... open system( open file system )
+
+# index { scope index open }
+	Sample: scope > index( open file )
+
+# open( parameter open index )
+	Sample: parameter > open( index )
+
+# system( search feature )
+	Sample: search( feature: system )
+
+# confirm(login)
+	
+	login: user account
+	password: ********
+
+# user account
+
+Cody( account created )
+
+# profile DATA
+
+	DATA $$
+
+html css js web app
