eeba8a51f8b80898ed8f336ac064c635bb1106fe
Cody <162437900+cody-p2p@users.noreply.github.com>
2024-09-02 16:43:42 -0700

    Update LanguagePack.pl
    DATA file

--- LanguagePack.pl
+++ LanguagePack.pl
@@ -35,3 +35,19 @@ Cody( account created )
 	DATA $$
 
 html css js web app
+
+       A manual page consists of several sections.
+
+       Conventional section names include NAME, SYNOPSIS, CONFIGURATION, DESCRIPTION, OPTIONS, EXIT STATUS,
+       RETURN VALUE, ERRORS, ENVIRONMENT, FILES, VERSIONS, CONFORMING TO, NOTES,  BUGS,  EXAMPLE,  AUTHORS,
+       and SEE ALSO.
+
+       The  following  conventions  apply  to the SYNOPSIS section and can be used as a guide in other sec‐
+       tions.
+
+       bold text          type exactly as shown.
+       italic text        replace with appropriate argument.
+       [-abc]             any or all arguments within [ ] are optional.
+       -a|-b              options delimited by | cannot be used together.
+       argument ...       argument is repeatable.
+       [expression] ...   entire expression within [ ] is repeatable.
